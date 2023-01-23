16fb7786c8b2d83c5e8f027f085aa78da3159a59
vinay <vinay@192.168.1.10>
2023-01-23 22:42:01 +0530

    Ali first commit

---
+++ Actitime/src/test/java/com/Actitime/Testscripts/CreateCustomerTest.java
@@ -0,0 +1,17 @@
+package com.Actitime.Testscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateCustomerTest {
+	@Test
+	public void createcustomer() {
+	
+		Reporter.log("customer created",true);
+	}
+
+}
